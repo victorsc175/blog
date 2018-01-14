35f50f0faac7a4a50573cb4bc4d3cd4e12280107
Victor Sc <victor.sc175@gmail.com>
2018-01-14 16:03:21 +0200

    Add styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index', to: 'home#index'
+  get 'about', to: 'home#about'
+  root 'home#index'
 end
