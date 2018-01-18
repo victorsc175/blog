46c53b1ce87a690c5e31d09728fded5e8e2a62fe
Victor Sc <victor.sc175@gmail.com>
2018-01-18 14:14:56 +0200

    Add named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'index', to: 'home#index'
-  get 'about', to: 'home#about'
+  get 'index', to: 'home#index', as: :home
+  get 'about', to: 'home#about', as: :about
   root 'home#index'
 end
