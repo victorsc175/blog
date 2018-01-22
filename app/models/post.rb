f48a54d8c829642838361d36e3c44847a8894295
Victor Sc <victor.sc175@gmail.com>
2018-01-22 13:26:37 +0200

    Add destroy dependency to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ApplicationRecord
   validates :title, :body, :user, presence: true
   validates :title, length: { in: 2..255 }
   belongs_to :user, counter_cache: true
-  has_many :comments, as: :commentable
-  has_many :marks
-  has_many :commentators, through: :comments, source: :user
-  has_one :seo, as: :seoable
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :marks, dependent: :destroy
+  has_many :commentators, through: :comments, source: :user, dependent: :destroy
+  has_one :seo, as: :seoable, dependent: :destroy
   
   # Use symbol like :moderator 
   def self.created_by(role)
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
                     uniqueness: true,
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
                               message: 'Invalid email' }
-  has_many :posts
-  has_many :comments
-  has_many :marks
-  has_many :commented_posts, through: :comments, source: :commentable, source_type: :Post
-  has_many :commented_users, through: :comments, source: :commentable, source_type: :User
-  has_one :seo, as: :seoable
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :marks, dependent: :destroy
+  has_many :commented_posts, through: :comments, source: :commentable, source_type: :Post, dependent: :destroy
+  has_many :commented_users, through: :comments, source: :commentable, source_type: :User, dependent: :destroy
+  has_one :seo, as: :seoable, dependent: :destroy
 end
