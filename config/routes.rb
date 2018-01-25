9f1e0f1728eb94a427f33e9d9e24d5568a75c9f9
Victor Sc <victor.sc175@gmail.com>
2018-01-25 12:34:22 +0200

    Add Post scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'index', to: 'home#index', as: :home
   get 'about', to: 'home#about', as: :about
