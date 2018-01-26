b6013f78ebd7c59195f18d0b8b3ae29841fe1072
Victor Sc <victor.sc175@gmail.com>
2018-01-26 18:00:23 +0200

    Refactor UI

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @comments = Comment.includes(:user).page(params[:page]).per(10)
   end
 
   # GET /posts/new
