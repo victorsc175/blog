19fd03b4623a68d33c1280d4031c91b2c21299b7
Victor Sc <victor.sc175@gmail.com>
2018-02-04 19:39:08 +0200

    Add puma prod config

---
+++ config/puma_prod.rb
@@ -0,0 +1,16 @@
+require 'dotenv'
+Dotenv.load('.env.local')
+
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 8))
+threads_count = Integer(ENV.fetch("MAX_THREADS", 1))
+threads(threads_count, threads_count)
+
+preload_app!
+
+rackup DefaultRackup
+port 8082
+environment 'production'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
